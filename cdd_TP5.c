e938019e6400ca6116ab5a598e40bd502cdbcfed
Franco di Pasquo <francodipasquo@mi.unc.edu.ar>
2025-06-12 21:01:39 -0300

    Modificacion define GPIOs - Script funcionando 100%

--- cdd_TP5.c
+++ cdd_TP5.c
@@ -5,6 +5,7 @@
 #include <linux/cdev.h>
 #include <linux/device.h>
 #include <linux/gpio.h>
+#include <linux/gpio/consumer.h>
 #include <linux/kthread.h>
 #include <linux/delay.h>
 #include <linux/mutex.h>
@@ -12,8 +13,9 @@
 
 #define DEVICE_NAME "cdd_TP5"
 #define CLASS_NAME  "cdd_TP5_class"
-#define GPIO24 24   // PIN 18 -> echo 0 | sudo tee /dev/cdd_TP5
-#define GPIO25 25   // PIN 22 -> echo 1 | sudo tee /dev/cdd_TP5
+// A partir del version de kernel 6.2, los pines GPIO ya no son tan directos como antes para direccionarse:
+#define GPIO24 536   // PIN 18 -> echo 0 | sudo tee /dev/cdd_TP5
+#define GPIO25 537   // PIN 22 -> echo 1 | sudo tee /dev/cdd_TP5
 #define SAMPLE_RATE 20000 // 20kHz
 #define ACQ_TIME_SEC 1
 #define N_SAMPLES (SAMPLE_RATE * ACQ_TIME_SEC)
@@ -26,6 +28,8 @@ static int selected_signal = 0; // 0 o 1
 static int *sample_buffer = NULL;
 static int acq_ready = 0;
 static DEFINE_MUTEX(signal_mutex);
+static struct gpio_desc *desc24 = NULL;
+static struct gpio_desc *desc25 = NULL;
 
 static int acquire_signal(int gpio)
 {
@@ -35,7 +39,14 @@ static int acquire_signal(int gpio)
 
     start = ktime_get();
     for (i = 0; i < N_SAMPLES; i++) {
-        sample_buffer[i] = gpio_get_value(gpio);
+        int value;
+        if (gpio == GPIO24 && desc24)
+            value = gpiod_get_value(desc24);
+        else if (gpio == GPIO25 && desc25)
+            value = gpiod_get_value(desc25);
+        else
+            value = 0;
+        sample_buffer[i] = value;
         if ((i%90 == 0) && (i<1000) ) // Solo los primeros algunos para no saturar el log
             printk(KERN_INFO "sample[%zu]=%d\n", i, sample_buffer[i]);
         // Espera activa para mantener la tasa de muestreo
@@ -130,6 +141,13 @@ static int __init mi_cdd_init(void) {
     gpio_direction_input(GPIO24);
     gpio_direction_input(GPIO25);
     printk(KERN_INFO "Configurados GPIO %d y %d como entradas\n", GPIO24, GPIO25);
+
+    desc24 = gpio_to_desc(GPIO24);
+    desc25 = gpio_to_desc(GPIO25);
+    if (!desc24 || !desc25) {
+        printk(KERN_ERR "No se pudo obtener descriptor GPIO\n");
+        return -EINVAL;
+    }
 
     sample_buffer = kmalloc_array(N_SAMPLES, sizeof(int), GFP_KERNEL);
     if (!sample_buffer) {
